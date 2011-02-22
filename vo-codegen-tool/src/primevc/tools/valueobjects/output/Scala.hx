a77b54c5bfc2ddf3153047d08676750204cb8fe5
Danny Wilson <danny@prime.vc>
2011-02-22 13:13:32 +0100

    Fix menu-button data missing in toXML() result. NamedSet toXML now returns NodeSeq of xml per item.

--- vo-codegen-tool/src/primevc/tools/valueobjects/output/Scala.hx
+++ vo-codegen-tool/src/primevc/tools/valueobjects/output/Scala.hx
@@ -760,7 +760,7 @@ file.writeString("
 		a("\n    vo");
 		a("\n  }");
 		
-		a("\n  def toXML(vo:"); a(def.name); a("VO): NodeSeq = if (vo.empty_?) NodeSeq.Empty else vo map(toXML(_)) reduceLeft { (a:NodeSeq, b:NodeSeq) => a union b };\n");
+		a("\n  def toXML(vo:"); a(def.name); a("VO): NodeSeq = if (vo.empty_?) NodeSeq.Empty else vo.toSeq.flatMap(toXML(_));\n");
 		
 /*		a("\n  override def get(key: String): Option["); a(type.name); a("] = {");
 		for (i in 0 ... def.propertiesSorted)

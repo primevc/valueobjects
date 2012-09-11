4de5d9f718d0ded773a5520a415781177e6579b2
Danny Wilson <danny@prime.vc>
2012-09-11 16:51:40 +0200

    Add VOCompiler Clojure output.

---
+++ vo-codegen-tool/src/primevc/tools/valueobjects/output/Clojure.hx
@@ -0,0 +1,57 @@
+package primevc.tools.valueobjects.output;
+ import primevc.tools.valueobjects.VODefinition;
+ import primevc.utils.NumberUtil;
+  using primevc.utils.TypeUtil;
+  using primevc.tools.valueobjects.output.ScalaUtil;
+  using primevc.tools.valueobjects.VODefinition;
+
+class ClojureDefVO implements CodeGenerator
+{
+	var file : sys.io.FileOutput;
+
+	public function new(file : sys.io.FileOutput) {
+		this.file = file;
+	}
+
+	public function genClass(def : ClassDef)
+	{
+		if (!def.isMixin) {
+			file.writeString("\n(defvo ");
+			file.writeString(def.fullName);
+			for (p in def.propertiesSorted) file.writeString(" :" + p.name);
+			file.writeString(")");
+		}
+	}
+
+	public function genEnum(def:EnumDef) {}
+
+	public function newModule(module:Module) : CodeGenerator {
+		module.generateWith(this);
+		return this;
+	}
+
+
+	public static function generate() for (m in Module.pkgRoots)
+	{
+		var parts = m.fullName.split(".");
+		var filename = parts.pop();
+		var dir = null;
+		for (p in parts) {
+			if (dir != null) dir += "/" + p; else dir = p;
+			try sys.FileSystem.createDirectory(dir);
+		}
+
+		filename = dir + "/" + filename + ".clj";
+		trace("WRITING: "+filename);
+		
+		var file = sys.io.File.write(filename, false);
+			file.writeString("(ns ");
+			file.writeString(m.fullName);
+			file.writeString("\n  (:use prime.vo.definition))\n");
+
+		var clj = new ClojureDefVO(file);
+		m.generateWith(clj);
+		
+		file.close();
+	}
+}

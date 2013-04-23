299e7b8b3e490fd7bdea3713b1d5c844bf10627c
Danny Wilson <danny@prime.vc>
2013-04-23 12:07:42 +0200

    Rename prime.vo/*proxy-map* to *deref-map* Improve documentation of deref-map semantics.

--- vo-scala-clojure/src/main/clojure/prime/vo.clj
+++ vo-scala-clojure/src/main/clojure/prime/vo.clj
@@ -9,12 +9,19 @@
 
 (set! *warn-on-reflection* true)
 
-(def ^:dynamic *proxy-map* "
-  A function from VOType => VOProxy. Used to fetch a ValueObject when dereferencing VORef properties.
+(def ^:dynamic *deref-map* "
+  Used to fetch a ValueObject when dereferencing VORef properties.
+  A function from VOType => deref-function.
+
+  Where deref-function is:
+    - A function from ID => ValueObject: (fn [id] vo...)
+    - Looks up an ID and returns the corresponding ValueObject.
+      (Can be a map, VOProxy or custom function.)
+
   Initially {}
 
   Usage:
-    (binding [*proxy-map* { MyReferencedValueObject (my-valueobject-proxy ...) }] @(:voref-field vo))
+    (binding [*deref-map* { MyReferencedValueObject (my-valueobject-proxy ...) }] @(:voref-field vo))
   " {})
 
 (def ^:dynamic *voseq-key-fn* "

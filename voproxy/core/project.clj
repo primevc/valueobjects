bb3bdd4896734b45a5731b7d7a37a2a33db8c918
Arnout Roemers <github@company.romeinszoon.nl>
2013-11-26 15:23:17 +0100

    Finish voproxy-core library build

--- voproxy/core/project.clj
+++ voproxy/core/project.clj
@@ -10,4 +10,17 @@
                  [cheshire "5.2.0"]
                  [fast-zip "0.3.0"]
                  [com.taoensso/timbre "2.6.2"]]
-  :profiles {:test {:dependencies [[midje "1.5.1"]]}})
+  :profiles {:test {:dependencies [[midje "1.5.1"]]}}
+  :pom-plugins [[com.theoryinpractise/clojure-maven-plugin "1.3.15"
+                 {:extensions "true"
+                  :executions ([:execution
+                                [:id "clojure-compile"]
+                                [:phase "compile"]
+                                [:configuration
+                                 [:temporaryOutputDirectory "true"]
+                                 [:sourceDirectories [:sourceDirectory "src"]]]
+                                [:goals [:goal "compile"]]]
+                               [:execution
+                                [:id "clojure-test"]
+                                [:phase "test"]
+                                [:goals [:goal "test"]]])}]])

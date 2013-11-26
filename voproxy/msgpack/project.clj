df8a2e6b89fc1c109cd93071eb9422ecbafdb7cf
Arnout Roemers <github@company.romeinszoon.nl>
2013-11-26 15:27:52 +0100

    Finish voproxy-msgpack library build

--- voproxy/msgpack/project.clj
+++ voproxy/msgpack/project.clj
@@ -7,4 +7,17 @@
   :url "https://github.com/primevc/valueobjects"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [prime/voproxy-core "0.1.0-SNAPSHOT"]
-                 [org.msgpack/msgpack "0.5.1-devel" :exclusions [org.slf4j/slf4j-log4j12]]])
+                 [org.msgpack/msgpack "0.5.1-devel"]]
+  :pom-plugins [[com.theoryinpractise/clojure-maven-plugin "1.3.15"
+                 {:extensions "true"
+                  :executions ([:execution
+                                [:id "clojure-compile"]
+                                [:phase "compile"]
+                                [:configuration
+                                 [:temporaryOutputDirectory "true"]
+                                 [:sourceDirectories [:sourceDirectory "src"]]]
+                                [:goals [:goal "compile"]]]
+                                 [:execution
+                                  [:id "clojure-test"]
+                                  [:phase "test"]
+                                  [:goals [:goal "test"]]])}]])
--- voproxy/msgpack/src/prime/vo/proxy/msgpack.clj
+++ voproxy/msgpack/src/prime/vo/proxy/msgpack.clj
@@ -2,7 +2,7 @@
 ;; License, v. 2.0. If a copy of the MPL was not distributed with this
 ;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-(ns prime.vo.proxy.messagepack
+(ns prime.vo.proxy.msgpack
   "A concrete implementation of the VOProxy protocol, with MessagePack
   as its backend."
   (:require [prime.vo.proxy :refer (VOProxy)]

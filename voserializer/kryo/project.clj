9c199caa89e0827209061fba2f5a9507c515c0d4
Arnout Roemers <github@company.romeinszoon.nl>
2013-11-27 17:10:34 +0100

    Finish voserializer-* builds + add POM aggregate

--- voserializer/kryo/project.clj
+++ voserializer/kryo/project.clj
@@ -2,11 +2,13 @@
 ;; License, v. 2.0. If a copy of the MPL was not distributed with this
 ;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-(defproject prime/filerepository-nfs "0.1.0-SNAPSHOT"
-  :description "An NFS implementation of FileRepository."
+(defproject prime/voserializer-kryo "0.1.0-SNAPSHOT"
+  :description "A Kryo serializer for ValueObjects."
   :url "https://github.com/primevc/valueobjects"
   :dependencies [[org.scala-lang/scala-library "2.9.2"]
-                 [prime/filerepository-core "0.1.0-SNAPSHOT"]]
+                 [prime/shared_2.9.2 "0.1-SNAPSHOT"]
+                 [com.esotericsoftware.kryo/kryo "2.20"]]
+  :profiles {:test {:dependencies [[org.specs2/specs2_2.9.2 "1.11"]]}}
   :pom-plugins [[net.alchim31.maven/scala-maven-plugin "3.1.6"
                  {:executions [:execution [:goals [:goal "compile"] [:goal "testCompile"]]]
                   :configuration ([:scalaVersion "2.9.2"]
@@ -14,4 +16,7 @@
                                   [:args [:arg "-Xelide-below"] [:arg "FINEST"]
                                          [:arg "-deprecation"] [:arg "-unchecked"]
                                          [:arg "-explaintypes"]]
-                                  [:jvmArgs [:arg "-XX:MaxPermSize=1024M"] [:arg "-Xmx4G"]])}]])
+                                  [:jvmArgs [:arg "-XX:MaxPermSize=1024M"] [:arg "-Xmx4G"]])}]
+
+                [com.mmakowski/maven-specs2-plugin "0.3.0"
+                 {:executions [:execution [:phase "test"] [:goals [:goal "run-specs"]]]}]])

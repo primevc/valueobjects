5d24daf7cfd98068cd1e31c3185daec28ebf6de3
Danny Wilson <danny@prime.vc>
2014-01-06 10:11:27 +0100

    Upgrade Apache Kafka to 0.8 - Add generic byte-array messagepack serializer and Kafka producer constructor.

--- vo-scala-clojure/src/main/scala/prime/utils/msgpack/KafkaVOSerializer.scala
+++ vo-scala-clojure/src/main/scala/prime/utils/msgpack/KafkaVOSerializer.scala
@@ -1,27 +1,43 @@
 package prime.utils.msgpack
- import prime.vo.ValueObject
  import prime.vo.source.ValueSource
  import java.io.{ByteArrayInputStream,ByteArrayOutputStream}
- import kafka.message.Message
+ import kafka.utils._
+ import kafka.producer._
+ import kafka.producer.async.DefaultEventHandler
  import kafka.serializer.Encoder
  import kafka.serializer.Decoder
- import org.msgpack.Unpacker
+ import org.msgpack.{MessagePackObject,Unpacker}
+ import org.msgpack.`object`._
 
-class KafkaVOSerializer extends Encoder[ValueObject] with Decoder[ValueSource] {
+object KafkaVOSerializer extends Encoder[Any] with Decoder[Any] {
 
-	def toMessage(event: ValueObject): Message = {
-		val out : ByteArrayOutputStream = new ByteArrayOutputStream()
-		val msgpack : VOPacker = new VOPacker(out)
-		msgpack.pack(event)
-		new Message(out.toByteArray())
-	}
+  def newProducer(config : ProducerConfig) = new kafka.producer.Producer(
+    config,
+    new DefaultEventHandler[Any,Any](config,
+                                     Utils.createObject[Partitioner[Any]](config.partitionerClass, config.props),
+                                     KafkaVOSerializer,
+                                     KafkaVOSerializer,
+                                     new ProducerPool(config)));
 
-	def toEvent(message: Message): ValueSource = {
-		val buf = message.payload
-		val arr = new Array[Byte] (buf.remaining)
-		buf.get(arr)
-		val msgUnpacker = new Unpacker(new ByteArrayInputStream (arr))
-		msgUnpacker.setVOHelper(VOUnpacker)
-		msgUnpacker.next.asInstanceOf[MessagePackValueSource]
-	}
+  def toBytes(vo: Any): Array[Byte] = {
+    val out : ByteArrayOutputStream = new ByteArrayOutputStream()
+    val msgpack : VOPacker = new VOPacker(out)
+    msgpack.pack(vo)
+    out.toByteArray()
+  }
+
+  def fromBytes(bytes: Array[Byte]): Any = {
+    val msgUnpacker = new Unpacker(new ByteArrayInputStream(bytes))
+    msgUnpacker.setVOHelper(VOUnpacker)
+    msgUnpacker.next.getData match {
+      case r : MessagePackValueSource => r
+      case r : ArrayType              => r.asArray
+      case r : BooleanType            => if (r.asBoolean) java.lang.Boolean.TRUE else java.lang.Boolean.FALSE
+      case r : IntegerType            => r.asLong
+      case r : FloatType              => r.asDouble
+      case r : MapType                => r.asMap
+      case r : NilType                => null
+      case r : RawType                => r.asString
+    }
+  }
 }

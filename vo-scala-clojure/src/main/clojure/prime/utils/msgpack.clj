8f956d3e52fc6f2f7c95344eda612e519f2d68d5
Danny Wilson <danny@prime.vc>
2014-03-21 19:41:10 +0100

    Fix reflection in `as-clojure

--- vo-scala-clojure/src/main/clojure/prime/utils/msgpack.clj
+++ vo-scala-clojure/src/main/clojure/prime/utils/msgpack.clj
@@ -2,22 +2,26 @@
 ;; License, v. 2.0. If a copy of the MPL was not distributed with this
 ;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-(ns prime.utils.msgpack)
+(ns prime.utils.msgpack
+  (:import [org.msgpack.object ArrayType BigIntegerTypeIMPL BooleanType
+                               DoubleTypeIMPL FloatTypeIMPL LongIntegerTypeIMPL MapType NilType
+                               RawType ShortIntegerTypeIMPL]
+           [prime.utils.msgpack MessagePackValueSource MessagePackObjectId]))
 
 (defn as-clojure
   "Get the Clojuresque type for the given MessagePackObject."
   [obj]
   (condp instance? obj
-    org.msgpack.object.ArrayType (reduce (fn [v o] (conj v (as-clojure o))) [] (.asArray obj))
-    org.msgpack.object.BigIntegerTypeIMPL (.asBigInteger obj)
-    org.msgpack.object.BooleanType (.asBoolean obj)
-    org.msgpack.object.DoubleTypeIMPL (.asDouble obj)
-    org.msgpack.object.FloatTypeIMPL (.asFloat obj)
-    org.msgpack.object.LongIntegerTypeIMPL (.asLong obj)
-    org.msgpack.object.MapType (reduce (fn [m [k v]] (assoc m (as-clojure k) (as-clojure v)))
-                                       {} (.asMap obj))
-    org.msgpack.object.NilType nil
-    org.msgpack.object.RawType (.asString obj) ;;---TODO: Is this always correct? Add option?
-    org.msgpack.object.ShortIntegerTypeIMPL (.asInt obj)
-    prime.utils.msgpack.MessagePackValueSource obj
-    prime.utils.msgpack.MessagePackObjectId (.oid obj)))
+    ArrayType              (reduce (fn [v o] (conj v (as-clojure o))) [] (.asArray ^ArrayType obj))
+    BigIntegerTypeIMPL     (.asBigInteger ^BigIntegerTypeIMPL obj)
+    BooleanType            (.asBoolean ^BooleanType obj)
+    DoubleTypeIMPL         (.asDouble ^DoubleTypeIMPL obj)
+    FloatTypeIMPL          (.asFloat ^FloatTypeIMPL obj)
+    LongIntegerTypeIMPL    (.asLong ^LongIntegerTypeIMPL obj)
+    MapType                (reduce (fn [m [k v]] (assoc m (as-clojure k) (as-clojure ^MapType v)))
+                                   {} (.asMap obj))
+    NilType                nil
+    RawType                (.asString ^RawType obj) ;;---TODO: Is this always correct? Add option?
+    ShortIntegerTypeIMPL   (.asInt ^ShortIntegerTypeIMPL obj)
+    MessagePackValueSource obj
+    MessagePackObjectId    (.oid ^MessagePackObjectId obj)))

7b91433fb1f359919e569ad3c6fb843543bb5e32
Arnout Roemers <github@functionalbytes.nl>
2014-05-28 15:40:36 +0200

    Decouple elasticsearch script from pathops

--- voproxy/elasticsearch/src/prime/vo/util/elasticsearch/script.clj
+++ voproxy/elasticsearch/src/prime/vo/util/elasticsearch/script.clj
@@ -7,7 +7,6 @@
   it is unnecessary to transfer the entire script for each request, at
   the end of this file the script functions are read and combined to
   smaller scripts."
-  (:require [prime.vo.pathops :as po])
   (:import [java.util List Map LinkedHashMap ArrayList]))
 
 
@@ -18,16 +17,57 @@
 
 ;;; Helper methods.
 
-(defmacro debug
-  [& args]
-  ;; `(println ~@args)
-  )
+(defmacro debug [& args] (when #_true false `(println ~@args)))
+
+
+(defn to=
+  [this that]
+  (= this that))
+
+
+(defn array-like?
+  [val]
+  (and val (or (sequential? val) (.. (class val) isArray) (instance? java.util.List val))))
+
+
+(defn concrete-path-step
+  [obj path-step]
+  (if (and (array-like? obj) (not (number? path-step)))
+    (let [[k v] (first path-step)
+          result (first (keep-indexed (fn [i item] (when (to= (get item k) v) i)) obj))]
+      (if-not result
+        (throw (IllegalArgumentException. (str "Could not find a match for '" path-step "'.")))
+        result))
+    #_else
+    path-step))
+
+
+(defn relative-vector-index [length i & options]
+  "Calculates the index relative to a vector length. Optionally, one
+  can specify that an extra index at the end is allowed, using the
+  :allow-index-after-last keyword. The relative index for a length of
+  0 is always 0. Examples:
+
+  (relative-vector-index 3 2)  ;=> 2
+  (relative-vector-index 3 4)  ;=> 2
+  (relative-vector-index 3 -1) ;=> 2
+  (relative-vector-index 3 -3) ;=> 0
+  (relative-vector-index 3 -4) ;=> 0
+  (relative-vector-index 0 0)  ;=> 0
+  (relative-vector-index 3 4 :allow-index-after-last)  ;=> 3
+  (relative-vector-index 3 -1 :allow-index-after-last) ;=> 3
+  (relative-vector-index 0 2 :allow-index-after-last)  ;=> 0"
+  (if (= 0 length)
+    0
+    (let [options (zipmap options (repeat true))
+          div (if (:allow-index-after-last options) (inc length) length)]
+      (if (>= i div) (dec div) (mod (max i (- div)) div)))))
 
 
 (defn- update-in-vo
   [obj path f & args]
   (debug "UPDATE IN VO" obj path f args)
-  (if-let [key (and path (po/concrete-path-step obj (first path)))]
+  (if-let [key (and path (concrete-path-step obj (first path)))]
     (let [inner (and obj (if (instance? Map obj) (.get ^Map obj key) #_else (.get ^List obj key)))
           value (apply update-in-vo inner (next path) f args)]
       (debug "BACKTRACK VALUE" value "- WILL BE PUT IN" obj "USING KEY" key)
@@ -48,9 +88,9 @@
   [^List obj from to]
   (debug "MOVE TO" obj from to)
   (let [from (->> from
-                  (po/concrete-path-step obj)
-                  (po/relative-vector-index (count obj)))
-        to (po/relative-vector-index (count obj) to)
+                  (concrete-path-step obj)
+                  (relative-vector-index (count obj)))
+        to (relative-vector-index (count obj) to)
         val (.remove obj (int from))]
     (doto obj (.add (int to) val))))
 
@@ -82,16 +122,16 @@
 (defn- replace-at*
   [obj at value]
   (debug "REPLACE AT" obj at value)
-  (let [at (po/concrete-path-step obj at)
-        at (if (and (po/array-like? obj) (number? at))
-             (po/relative-vector-index (count obj) at)
+  (let [at (concrete-path-step obj at)
+        at (if (and (array-like? obj) (number? at))
+             (relative-vector-index (count obj) at)
              at)]
-    (if (po/array-like? obj)
+    (if (array-like? obj)
       (.remove ^List obj (int at))
       (.remove ^Map obj at))
-    (cond (and (po/array-like? obj) (po/array-like? value))
+    (cond (and (array-like? obj) (array-like? value))
           (.addAll ^List obj at value)
-          (po/array-like? obj)
+          (array-like? obj)
           (.add ^List obj at value)
           :else
           (.put ^Map obj at value)))
@@ -109,11 +149,11 @@
 (defn- remove-from*
   [obj at]
   (debug "REMOVE FROM" obj at)
-  (let [at (po/concrete-path-step obj at)
-        at (if (and (po/array-like? obj) (number? at))
-             (po/relative-vector-index (count obj) at)
+  (let [at (concrete-path-step obj at)
+        at (if (and (array-like? obj) (number? at))
+             (relative-vector-index (count obj) at)
              at)]
-    (if (po/array-like? obj)
+    (if (array-like? obj)
       (.remove ^List obj (int at))
       (.remove ^Map obj at)))
   obj)
@@ -130,10 +170,10 @@
 (defn- insert-at*
   [^List obj at value]
   (debug "INSERT AT" obj at value)
-  (let [at (po/concrete-path-step obj at)
+  (let [at (concrete-path-step obj at)
         obj (or obj (new ArrayList))
-        at (po/relative-vector-index (count obj) at)]
-    (if (po/array-like? value)
+        at (relative-vector-index (count obj) at)]
+    (if (array-like? value)
       (.addAll ^List obj at value)
       (.add ^List obj at value))
     obj))
@@ -151,7 +191,7 @@
   [^List obj ^Map value]
   (debug "APPEND TO" obj value)
   (let [obj (or obj (new ArrayList))]
-    (if (po/array-like? value)
+    (if (array-like? value)
       (.addAll ^List obj value)
       (.add ^List obj value))
     obj))
@@ -221,47 +261,39 @@
 ;;; Per operation script source strings.
 
 (def base-script
-  (str (ns-source prime.utils)
-       (ns-source prime.vo.pathops)
-       (def-source prime.vo.pathops/array-like?)
-       (def-source prime.vo.pathops/concrete-path-step)
-       (def-source prime.vo.pathops/relative-vector-index)
-       (ns-source prime.vo.util.elasticsearch.script)
+  (str (ns-source prime.vo.util.elasticsearch.script)
+       (def-source prime.vo.util.elasticsearch.script/array-like?)
+       (def-source prime.vo.util.elasticsearch.script/concrete-path-step)
+       (def-source prime.vo.util.elasticsearch.script/relative-vector-index)
        (def-source prime.vo.util.elasticsearch.script/debug)
        (def-source prime.vo.util.elasticsearch.script/update-in-vo)))
 
 (def move-to-script
   (str base-script
-       (ns-source prime.vo.util.elasticsearch.script)
        (def-source prime.vo.util.elasticsearch.script/move-to*)
        (def-source prime.vo.util.elasticsearch.script/move-to)))
 
 (def merge-at-script
   (str base-script
-       (ns-source prime.vo.util.elasticsearch.script)
        (def-source prime.vo.util.elasticsearch.script/merge-at*)
        (def-source prime.vo.util.elasticsearch.script/merge-at)))
 
 (def replace-at-script
   (str base-script
-       (ns-source prime.vo.util.elasticsearch.script)
        (def-source prime.vo.util.elasticsearch.script/replace-at*)
        (def-source prime.vo.util.elasticsearch.script/replace-at)))
 
 (def remove-from-script
   (str base-script
-       (ns-source prime.vo.util.elasticsearch.script)
        (def-source prime.vo.util.elasticsearch.script/remove-from*)
        (def-source prime.vo.util.elasticsearch.script/remove-from)))
 
 (def insert-at-script
   (str base-script
-       (ns-source prime.vo.util.elasticsearch.script)
        (def-source prime.vo.util.elasticsearch.script/insert-at*)
        (def-source prime.vo.util.elasticsearch.script/insert-at)))
 
 (def append-to-script
   (str base-script
-       (ns-source prime.vo.util.elasticsearch.script)
        (def-source prime.vo.util.elasticsearch.script/append-to*)
        (def-source prime.vo.util.elasticsearch.script/append-to)))

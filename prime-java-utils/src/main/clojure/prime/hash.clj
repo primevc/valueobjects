fb58024aac67cc6f49598d48b60957dd82d95bb0
Danny Wilson <danny@prime.vc>
2015-09-08 15:34:30 +0200

    Add url-safe-base64 option

--- prime-java-utils/src/main/clojure/prime/hash.clj
+++ prime-java-utils/src/main/clojure/prime/hash.clj
@@ -10,9 +10,12 @@
 
 (defn hmac-sha1->base64
   "Calculate Base64 encoded HMAC-SHA1 signature for given data."
-  [^String key ^String data]
-  (let [secret (SecretKeySpec. (.getBytes key "UTF-8") "HmacSHA1")
-        mac    (doto (Mac/getInstance "HmacSHA1") (.init secret))
-        hash   (.doFinal mac (.getBytes data))
-        base64 (org.apache.commons.codec.binary.Base64/encodeBase64 hash)]
-    (String. base64 "US-ASCII")))
+  ([^String key ^String data]
+    (hmac-sha1->base64 key data false))
+
+  ([^String key ^String data ^Boolean url-safe?]
+    (let [secret (SecretKeySpec. (.getBytes key "UTF-8") "HmacSHA1")
+          mac    (doto (Mac/getInstance "HmacSHA1") (.init secret))
+          hash   (.doFinal mac (.getBytes data))
+          base64 (org.apache.commons.codec.binary.Base64/encodeBase64 hash false url-safe?)]
+      (String. base64 "US-ASCII"))))

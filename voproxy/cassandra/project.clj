e37122019a9e3f9d9763c35c931dcccf79ddb5f2
Danny Wilson <danny@prime.vc>
2016-04-21 13:40:21 +0200

    Match naming of other VO proxy libraries

--- voproxy/cassandra/project.clj
+++ voproxy/cassandra/project.clj
@@ -2,7 +2,7 @@
 ;; License, v. 2.0. If a copy of the MPL was not distributed with this
 ;; file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-(defproject prime/voproxy.cassandra-history "0.1.0-SNAPSHOT"
+(defproject prime/voproxy-cassandra-history "0.1.0-SNAPSHOT"
   :description "Cassandra implementation of VOProxy."
   :url "https://github.com/primevc/valueobjects"
   :dependencies [[prime/voproxy-core "0.1.0-SNAPSHOT"]
